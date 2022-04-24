73af1e57cbec082614bd2b41043cb3e7f7ba5300
nmachuca <nmachuca@protonmail.com>
2022-04-24 12:55:05 -0230

    feat: referral system

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Traits\Referable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Referable;
 
     /**
      * The attributes that are mass assignable.
