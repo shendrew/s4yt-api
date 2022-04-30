44648d08c9799f1c8ccc927ca0c3b1a6a9f8de4e
nmachuca <nmachuca@protonmail.com>
2022-04-30 19:24:37 -0230

    feat: roles dependency implementation

---
+++ app/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App;
+
+
+class Role
+{
+    const SUPER_ADMIN = 'super_admin';
+    const ADMIN = 'admin';
+    const BUSINESS_PARTNER = 'business_partner';
+    const RAFFLE_PARTNER = 'raffle_partner';
+    const PLAYER = 'player';
+    const BU_PLAYER = 'bu_player';
+}
--- config/app.php
+++ config/app.php
@@ -2,6 +2,18 @@
 
 return [
 
+    'super_admin' => [
+        'name' => env('SUPER_ADMIN_NAME', 'super_admin'),
+        'email' => env('SUPER_ADMIN_EMAIL', 'superadmin@s4yt.com'),
+        'password' => env('SUPER_ADMIN_PASS', 'qwerty123'),
+    ],
+
+    'admin' => [
+        'name' => env('ADMIN_NAME', 'admin'),
+        'email' => env('ADMIN_EMAIL', 'admin@s4yt.com'),
+        'password' => env('ADMIN_PASS', 'qwerty123'),
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Application Name
