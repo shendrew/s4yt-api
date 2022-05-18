4982fbca679451dae46350111270aa4042d8d771
nmachuca <nmachuca@protonmail.com>
2022-05-18 15:13:00 -0230

    feat: coins table

---
+++ app/Services/PlayerService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+use App\Coin;
+use App\Configuration;
+use App\Role;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class PlayerService
+{
+
+    /**
+     * Player registration
+     *
+     * @param array $data
+     * @return User
+     */
+    public function addPlayer(array $data): User
+    {
+        // Insert record in table users
+        $player = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        // Assign role
+        $player->assignRole(Role::PLAYER);
+
+        // Add default coins
+        $this->addCoinsToStudent(Configuration::getValueByKey(Configuration::INITIAL_COINS), $player->id);
+
+        // Return created object
+        return $player;
+    }
+
+    /**
+     * Add coins records for the student
+     *
+     * @param int $coins
+     * @param string $player_id
+     */
+    private function addCoinsToStudent(int $coins, string $player_id)
+    {
+        factory(Coin::class, $coins)->create(['user_id' => $player_id]);
+    }
+}
