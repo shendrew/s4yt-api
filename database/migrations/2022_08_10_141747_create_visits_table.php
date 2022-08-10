13a2d66aba76529ae469458c74d1a9156e80cf75
Andrew S <andrew190@gmail.com>
2022-08-10 16:07:16 -0400

    feat: question, answer, visit migrations

---
+++ database/migrations/2022_08_10_141747_create_visits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('player_id')->constrained('players');
+            $table->foreignId('event_partner_id')->constrained('event_partners');
+            $table->dateTime('last_visited')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+}
