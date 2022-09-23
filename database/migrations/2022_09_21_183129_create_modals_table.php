8b4e07fba8625facd54e84cd28fe009227f33027
Andrew S <andrew190@gmail.com>
2022-09-23 18:36:03 -0400

    feat: modals db

---
+++ database/migrations/2022_09_21_183129_create_modals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modals', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content-type');
+            $table->text('instructions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modals');
+    }
+}
